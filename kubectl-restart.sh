67eb889afb48047267e71e47d358ce8d60df504e
vahit <vahid.maani@gmail.com>
2018-10-15 22:11:57 +0330

    refactor code and update readme file

--- kubectl-restart.sh
+++ kubectl-restart.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-CURRENT_SCALE=""
-GOAL_SCALE=""
-NAMESPACE=""
-DEP_NAME=""
-DEP_LIST=""
-declare -a EXCEPT
-
-while getopts ":n:o:e:s:r" opt; do
+while getopts ":n:o:e:" opt; do
     case $opt in
         n) NAMESPACE=${OPTARG};;
         e) EXCEPT=${OPTARG};;
         o) DEPS_LIST=${OPTARG};;
-        s) GOAL_SCALE="${OPTARG}";;
-        r) RESTART="TRUE"
-           GOAL_SCALE=0;;
         :) echo "Option -${OPTARG} requires an argument." >&2
            exit 1;;
         \?) echo "Invalid options: ${OPTARG}" >&2
@@ -22,39 +12,25 @@ while getopts ":n:o:e:s:r" opt; do
     esac
 done
 
-function scale(){
-    NAMES=${1}        # Namespace.
-    DEPN=${2}         # Deployment name.
-    CURRENT=${3}      # Current scale.
-    GOAL=${4}         # Goal scale.
-    OUTPUT=$(kubectl --namespace "${NAMES}" scale --current-replicas="${CURRENT}" --replicas="${GOAL}" deployment/"${DEPN}")
-    RETURN_CODE=${?}
-    if [[ ${RETURN_CODE} -eq 0 ]]; then
-        echo "\"${DEPN}\" scaled to ${GOAL} successfuly."
-    else
-        echo -e "\e[5mError\e[0m: ${DEP_NAME} could not scale DOWN, ${OUTPUT}"
-        exit 1
-    fi
+NAMESPACE=${NAMESPACE:-default}
+
+function restart(){
+    POD_LIST=$(kubectl --namespace "${NAMESPACE}" get pod --output custom-columns=NAME:.metadata.name --no-headers | grep "^${EACH_DEP}")
+    for EACH_POD in ${POD_LIST}; do
+        kubectl --namespace "${NAMESPACE}" delete pod "${EACH_POD}"
+        sleep 2
+    done
+    unset POD_LIST
 }
 
-if [[ -z ${NAMESPACE} ]]; then
-    echo "You should be specified namespace."
-    exit 1
-fi
-
 if [[ -z ${DEPS_LIST} ]]; then
-    DEPS_LIST=$(kubectl --namespace "${NAMESPACE}" get deployment | grep -v NAME | awk '{print $1}')
+    # That means we want restart all of deployments in the namespace.
+    DEPS_LIST=$(kubectl --namespace "${NAMESPACE}" get deployment --output custom-columns=NAME:.metadata.name --no-headers )
 fi
 
 for EACH_DEP in ${DEPS_LIST}; do
     if [[ "${EXCEPT[@]}" != *"${EACH_DEP}"* ]]; then
-        CURRENT_SCALE=$(kubectl --namespace "${NAMESPACE}" get deployment "${EACH_DEP}" --output json | jq '.spec.replicas')
-        if [[ ${RESTART} == "TRUE" ]]; then
-            scale "${NAMESPACE}" "${EACH_DEP}" "${CURRENT_SCALE}" "${GOAL_SCALE}"
-            scale "${NAMESPACE}" "${EACH_DEP}" "${GOAL_SCALE}" "${CURRENT_SCALE}"
-        else
-            scale "${NAMESPACE}" "${EACH_DEP}" "${CURRENT_SCALE}" "${GOAL_SCALE}"
-        fi
+        restart "${EACH_DEP}"
     fi
 done
 
--- readme.md
+++ readme.md
@@ -1,23 +1,17 @@
 # Kubectl Restarter
-Restart/Scale DOWN and UP it again a whole of deployments in a namespace or specific deployment.
-You can Exclude some deployments and you can use this script to only Scale UP or Scale DOWN (specific or all of)Deployments in namespace.
+Restart a whole of a namespace or specific deployments in it. You can Exclude some deployments too.
 
 ### Options:
-+ **n**: Namespace (*REQUIRED*)
++ **n**: Namespace (defauotl value=**default**)
 + **e**: Exclude one or more deployments. this option argument is an *array of deployments.*
-+ **o**: Deployment name, If you wand work on one specific deployment it's *REQUIRED*. 
-+ **s**: Scale UP/DOWN, If you wand scale up or down deployment you need this option.
-+ **r**: Restart one or more deployment. This option Don't has any argument.
-
-#### Note:
-You should use as one of **r** or **s** options.
++ **o**: You can specific deployments list with this option.
 
 #### Examples:
++ Restart all of deployments in *default* namespace:
+```kubectl-restart.sh```
 + Restart all of deployments in *testing* namespace:
-`kubectl-restart -n testing`
+```kubectl-restart -n testing```
 + Restart all of deployments in *testing* namespace except *mysql* and *redis* deployments:
-`kubectl-restart -n testing -e "mysql redis"`
-+ Scale *mysql* deployment to 5 from *testing* namespace.
-`kubectl-restart -n testing -s 5 -o mysql`
+```kubectl-restart -n testing -e "mysql redis"```
 and ...
 
